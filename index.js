d694bcb7a78bcaffb85815798d877c0b99223448
Rwan <rabdelmonem0@gmail.com>
2021-04-14 11:45:29 -0700

    fixed spelling

--- index.js
+++ index.js
@@ -9,42 +9,42 @@ const questions = [
     {
         type: 'input',
         name: 'Title',
-        messgae: 'What is the title of your project?'
+        message: 'What is the title of your project?'
     },
     {
         type: 'input',
         name: 'Description',
-        messgae: 'Write a description of your project:'
+        message: 'Write a description of your project:'
     },
     {
         type: 'input',
         name: 'Installation',
-        messgae: 'What steps are required to install your project?'
+        message: 'What steps are required to install your project?'
     },
     {
         type: 'input',
         name: 'Usage',
-        messgae: 'Please provide instructions and examples for use. Iclude screenshots if needed'
+        message: 'Please provide instructions and examples for use. Iclude screenshots if needed'
     },
     {
         type: 'input',
         name: 'Contribution',
-        messgae: 'List all collaborators, if any:'
+        message: 'List all collaborators, if any:'
     },
     {
         type: 'input',
         name: 'Tests',
-        messgae: 'Write tests for the application and examples of how to run them:'
+        message: 'Write tests for the application and examples of how to run them:'
     },
     {
         type: 'input',
         name: 'GithubURL',
-        messgae: 'What is your Github URL?'
+        message: 'What is your Github URL?'
     },
     {
         type: 'input',
         name: 'Email',
-        messgae: 'What is your email?'
+        message: 'What is your email?'
     },
     {
         type: 'list',
